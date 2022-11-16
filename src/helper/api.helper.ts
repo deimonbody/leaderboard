1b0facc0d4f1782ff75f4d562ab02f067a103d4a
unknown <pavlov.d@donnu.edu.ua>
2022-11-16 12:14:45 +0200

    commit after review

--- src/helper/api.helper.ts
+++ src/helper/api.helper.ts
@@ -4,27 +4,35 @@ import { proccesingUserData } from './user.helper';
 
 export const GET_USERS_API_PATH = 'http://coding-test.cube19.io/frontend/v1/starting-state';
 export const POST_USER_API_PATH = 'http://coding-test.cube19.io/frontend/v1/process-user';
+
 export const getUsers = async ():Promise<IUser[] | undefined> => {
-  try {
-    const response = await axios.get(GET_USERS_API_PATH);
-    if (response.status === 200) {
-      return proccesingUserData(response.data);
-    }
-  } catch {
-    return getUsers();
-  }
+  const response = await axios.get(GET_USERS_API_PATH);
+  return proccesingUserData(response.data);
 };
 export const addNewUserAPI = async (userName:string):Promise<INotFulluser | undefined> => {
-  try {
-    const response = await axios.post(POST_USER_API_PATH, {
-      'username': userName,
-    });
-    if (response.status === 200) {
-      return {
-        name: response.data['display-name'] as string,
-      };
-    }
-  } catch {
-    return addNewUserAPI(userName);
-  }
+  const response = await axios.post(POST_USER_API_PATH, {
+    'username': userName,
+  });
+
+  return {
+    name: response.data['display-name'] as string,
+  };
 };
+
+axios.interceptors.response.use((res) => res, async (err) => {
+  const { status } = err.response;
+  const { responseURL } = err.request;
+  if (status === 500) {
+    if (responseURL === GET_USERS_API_PATH) {
+      const result = await axios.get(GET_USERS_API_PATH);
+      return result;
+    }
+    if (responseURL === POST_USER_API_PATH) {
+      const responseData = JSON.parse(err.response.config.data);
+      const result = await axios.post(POST_USER_API_PATH, {
+        'username': responseData.username,
+      });
+      return result;
+    }
+  }
+});
